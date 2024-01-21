c35683768032aa01b297059bcadbe854dc12d5af
forsmsmail2019 <113799599+forsmsmail2019@users.noreply.github.com>
2024-01-20 22:25:46 -0500

    check if is text else puts message

--- ceaser_cipher.rb
+++ ceaser_cipher.rb
@@ -3,4 +3,19 @@ def ceasar_cipher(text,key)
   cap_letter = small_letter.map do |letter|
     letter.upcase
   end
+
+  #check if the factor number is numeric
+  if !key.is_a? Numeric
+    puts "please enter a shift factor number"
+    return
+  end
+
+  #check if the text is text
+  if text.is_a? String
+
+  return result
+else
+    puts "please enter a text"
+end
+
 end
