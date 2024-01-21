be7e47b739115819730f58e0534b0cad2dfbd6b5
forsmsmail2019 <113799599+forsmsmail2019@users.noreply.github.com>
2024-01-20 22:30:08 -0500

    check if char is cap and do calcul

--- ceaser_cipher.rb
+++ ceaser_cipher.rb
@@ -1,4 +1,5 @@
 def ceasar_cipher(text,key)
+  result = ""
   small_letter = "abcdefghijklmnopqrstuvwxyz".split("")
   cap_letter = small_letter.map do |letter|
     letter.upcase
@@ -12,7 +13,37 @@ def ceasar_cipher(text,key)
 
   #check if the text is text
   if text.is_a? String
+    #p small_letter
+    #p cap_letter
+    # check with any char in the entred text if its cap or not
+    text.each_char {|c|
+    # if capital latter
+    if cap_letter.include?(c)
+      #print "#{c.ord} is capital letter "
+      #if the factor number is more than 25
+      if (c.ord - 65 + key) > 25
+        shift = (c.ord - 65 + key) % 26
+        #puts "it will shift by #{shift}"
+        tmp_letter = cap_letter[shift]
+      else
+        tmp_letter = cap_letter[c.ord - 65 + key]
+      end
+      #puts tmp_letter
+      result +=tmp_letter
+    else
+      #print "#{c} is not capital letter "
+      if (c.ord - 97 + key) > 25
+        shift = (c.ord - 97 + key) % 26
+        #puts "it will shift by #{shift}"
+        tmp_letter = small_letter[shift]
+      else
+        tmp_letter = small_letter[c.ord - 97 + key]
+      end
+      #puts tmp_letter
+      result +=tmp_letter
+    end
 
+  }
   return result
 else
     puts "please enter a text"
